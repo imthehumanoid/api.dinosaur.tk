f6e5a11934efb4587845be549292c5a6aba744ab
imthehumanoid <83677145+imthehumanoid@users.noreply.github.com>
2022-01-03 11:31:16 -0600

    Create trivia.js

---
+++ trivia.js
@@ -0,0 +1,110 @@
+module.exports = [
+  {
+    question: 'What is the dinosaur with the longest name?',
+    correct: 'Micropachycephalosaurus',
+    incorrect: ['Charcharodontosaurus', 'Archaeornithommimmus', 'Tyrannosaurus Rex', 'Velociraptor'],
+    difficulty: 1
+  },
+  {
+    question: 'What is the dinosaur with the shortest name?',
+    correct: 'Yi',
+    incorrect: ['Mei', 'Ih', 'Khaan', 'Minmi'],
+    difficulty: 1
+  },
+  {
+    question: 'What is someone who studies dinosaurs called?',
+    correct: 'Paleontologist',
+    incorrect: ['Archeologist', 'Geologist', 'Scientist', 'Historian'],
+    difficulty: 1
+  },
+  {
+    question: 'When was the novel Jurassic Park published?',
+    correct: '1990',
+    incorrect: ['1942', '1993', '1995', '2015'],
+    difficulty: 1
+  },
+  {
+    question: 'Who wrote the novel Jurassic Park?',
+    correct: 'Michael Crichton',
+    incorrect: ['Steven Spielberg', 'Colin Trevorrow', 'Chris Pratt', 'John Hammond'],
+    difficulty: 1
+  },
+  {
+    question: 'When was the novel Jurassic Park published?',
+    correct: '1990',
+    incorrect: ['1942', '1993', '1995', '2015'],
+    difficulty: 1
+  },
+  {
+    question: 'Who created the word "dinosaur"?',
+    correct: 'Sir Richard Owen',
+    incorrect: ['Alan Grant', 'William Buckland', 'William Parker Foulke', 'Robert Plot'],
+    difficulty: 1
+  },
+  {
+    question: 'What was the first dinosaur fossil discovered?',
+    correct: 'Megalosaurus femur',
+    incorrect: ['Tyrannosaurus Rex tooth', 'Velociraptor claw', 'Styracosaurus ankle', 'Brachiosaurus femur'],
+    difficulty: 1
+  },
+  {
+    question: 'When was the first dinosaur fossil discovered?',
+    correct: '1677',
+    incorrect: ['1202', '1990', '1824', '1942'],
+    difficulty: 1
+  },
+  {
+    question: 'What does the word "dinosaur" mean?',
+    correct: 'Terrible lizard',
+    incorrect: ['Awful bird', 'Tyrant lizard', 'Swift thief', 'Gaint lizard'],
+    difficulty: 1
+  },
+  {
+    question: 'What is the first dinosaur on screen in the movie Jurassic Park?',
+    correct: 'Velociraptor',
+    incorrect: ['Brachiosaurus', 'Tyrannosaurus Rex', 'Dilophosaurus', 'Alamosaurus'],
+    difficulty: 1
+  },
+  {
+    question: 'In the movie Jurassic Park, what is the non-carinvorous dinosaur skeleton next to the staircase in the Visitor Center?',
+    correct: 'Alamosaurus',
+    incorrect: ['Brachiosaurus', 'Apatosaurus', 'Tyrannosaurus Rex', 'Spinosaurus'],
+    difficulty: 1
+  },
+  {
+    question: 'How tall is a velociraptor?',
+    correct: '1.6 feet (0.5 meters)',
+    incorrect: ['6.8 feet (2 meters)', '5 feet (1.5 meters)', '20 feet (6.1 meters)', '12 feet (3.7 meters)'],
+    difficulty: 1
+  },
+  {
+    question: 'How many horns did triceratops have?',
+    correct: '3',
+    incorrect: ['1', '2', '4', '5'],
+    difficulty: 1
+  },
+  {
+    question: 'Dinosaurs appeared in first in which geological period?',
+    correct: 'Triassic',
+    incorrect: ['Jurassic', 'Cretaceous', 'Permian', 'Cambrain'],
+    difficulty: 1
+  },
+  {
+    question: 'What was diplodocus\' diet?',
+    correct: 'Herbavore',
+    incorrect: ['Carnivore', 'Piscovore', 'Omnivore', 'Frugivore'],
+    difficulty: 1
+  },
+  {
+    question: 'What dinosaur has the smallest brain for it\'s body size?',
+    correct: 'Stegosaurus',
+    incorrect: ['Tyrannosaurus Rex', 'Triceratops', 'Compsognathus', 'Microraptor'],
+    difficulty: 1
+  },
+  {
+    question: '',
+    correct: '',
+    incorrect: [],
+    difficulty: 1
+  },
+]
